61839890815233b50c66c09c00ee711c72ab2c40
byteyoo <dyoo3@albany.edu>
2017-06-23 23:44:24 -0400

    End of the first day

---
+++ api_keys.py
@@ -0,0 +1,3 @@
+
+BING_KEY = "efedf1846d364fc0ac8eed301c9f19c4"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+SPELL_KEY = "865dc4469842436e8f378c9cfbf77846" #LMAO THIS IS SPELL CHECK API
