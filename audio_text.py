f0f5fc5b471023872a1c3819c0070617920e6cdb
janetchang <janetlaichang@gmail.com>
2017-06-24 12:47:36 -0400

    troubleshooting 401 error

--- audio_text.py
+++ audio_text.py
@@ -7,18 +7,25 @@ import timeit
 import logging
 # obtain path to "english.wav" in the same folder as this script
 from os import path
+import os
 from api_keys import BING_KEY
 from api_keys import SPELL_KEY
-requestSpeech = requests.get('https://api.cognitive.microsoft.com/sts/v1.0') #SPEECH
+from api_keys import SUBSCRIPTION_KEY
+from monotonic import monotonic
+
+#requestSpeech = requests.get('https://api.cognitive.microsoft.com/sts/v1.0', auth=("janetlaichang@gmail.com", "Ww176162036")) #SPEECH
 #requestSpell = requests.get('https://api.cognitive.microsoft.com/bing/v5.0/spellcheck') # SPELL
 
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "example.wav")
 # AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
 # AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
 
+class RequestError(Exception):
+	pass
+
 class BingSpeechAPI:
-	def __init__(self, key=os.getenv('BING_KEY', '')):
-		self.key = key
+	def __init__(self): #, key=SUBSCRIPTION_KEY):
+		self.key = SUBSCRIPTION_KEY
 		self.access_token = None
 		self.expire_time = None
 		self.session = requests.Session()
@@ -30,6 +37,7 @@ class BingSpeechAPI:
 			headers = {"Ocp-Apim-Subscription-Key": self.key}
         	start_time = monotonic()
         	response = self.session.post("https://api.cognitive.microsoft.com/sts/v1.0/issueToken", headers=headers)
+        	print ("This is the response", response)
 
 		if response.status_code != 200:
 			raise RequestError("http request error with status code {}".format(response.status_code))
@@ -42,6 +50,7 @@ class BingSpeechAPI:
 		r = sr.Recognizer()
 		with sr.AudioFile(AUDIO_FILE) as source:
 			audio = r.record(source)  # read the entire audio file
+		print ("Get audio file result", audio)
 
 	def recognize_speech():
 		# recognize speech using Microsoft Bing Voice Recognition
@@ -55,9 +64,13 @@ class BingSpeechAPI:
 def main():
     logging.basicConfig(level=logging.DEBUG)
     bing = BingSpeechAPI()
+    bing.authenticate()
+    bing.get_audio_file()
+    bing.recognize_speech()
 
 if __name__ == '__main__':
 	main()
+
 # Text to Slang Transcribe
 
 
